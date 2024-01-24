53eefe5103ee0e7f59b08a358bd00d8eb222aedd
EC2 Default User <ec2-user@ip-10-1-0-99.ap-northeast-1.compute.internal>
2024-01-24 11:20:38 +0000

    first

--- app.rb
+++ app.rb
@@ -3,12 +3,13 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require './models.rb'
 require 'line/bot'
+require "openai"
 get '/' do
   erb :index
 end
 
 
- def client
+def client
         @client ||= Line::Bot::Client.new { |config|
           config.channel_secret = "a9a7928fd25356ca14b0e0aa05b6568c"
           config.channel_token = "uOSv2yphg2AkqPJOKJd6et3jVEA+YTwlKUflGvCikdDW3T81UBiOtsnkfGfmZps4uoaL2HPn4yha2CnidLe8cTHv1xLINVqDAVlBWcUqIof98V/SFHG5ShXxTxrd2/lXhypHaUfMsm2AXeZTtDCWUAdB04t89/1O/w1cDnyilFU="
@@ -30,9 +31,18 @@ post '/callback' do
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
+
+          chatgpt = OpenAI::Client.new(access_token: "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")
+
+          response = chatgpt.chat(
+              parameters: {
+                  model: "gpt-3.5-turbo",
+                  messages: [{ role: "user", content: event.message['text'] }],
+              })
+          
           message = {
             type: 'text',
-            text: 'あ'
+            text: response.dig("choices", 0, "message", "content")
           }
         end
       end
