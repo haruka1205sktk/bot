34b8a71176a49e00c1df5174e39e33fde0595bfe
EC2 Default User <ec2-user@ip-10-1-14-209.ap-northeast-1.compute.internal>
2024-02-14 10:55:16 +0000

    変更

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ end
 
 def client
         @client ||= Line::Bot::Client.new { |config|
-          config.channel_secret = ENV["CHANNEL_SECRET"]
-          config.channel_token = ENV["CHANNEL_TOKEN"]
+          config.channel_secret = "a9a7928fd25356ca14b0e0aa05b6568c"
+          config.channel_token = "uOSv2yphg2AkqPJOKJd6et3jVEA+YTwlKUflGvCikdDW3T81UBiOtsnkfGfmZps4uoaL2HPn4yha2CnidLe8cTHv1xLINVqDAVlBWcUqIof98V/SFHG5ShXxTxrd2/lXhypHaUfMsm2AXeZTtDCWUAdB04t89/1O/w1cDnyilFU="
         }
 end
  
@@ -58,8 +58,8 @@ post '/callback' do
               parameters: {
                 model: "gpt-3.5-turbo",
                   messages: [
-                    # { role: "system", content: languageData.language + "に" + "翻訳して下さい" },
-                    { role: "user", content: event.message['text']+"を英語に変更して" }
+                    { role: "system", content: languageData.language + "に" + "翻訳して下さい" },
+                    { role: "user", content: event.message['text'] }
                   ]
               }
             )
